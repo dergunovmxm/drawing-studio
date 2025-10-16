ba174a7ccbd6a9413afb869592164f55065d009f
maksim.dergunov <maksim.dergunov@restream.ru>
2025-10-16 17:15:22 +0400

    fix

--- src/pages/Main/description.js
+++ src/pages/Main/description.js
@@ -1,30 +1,30 @@
 export const headers = [
   {
     alias: "znaki",
-    link: "public/headersImage/znaki.jpg",
+    link: "headersImage/znaki.jpg",
   },
   {
     alias: "poetry",
-    link: "public/headersImage/poetry.jpg",
+    link: "headersImage/poetry.jpg",
   },
   {
     alias: "free",
-    link: "public/headersImage/free.jpg",
+    link: "headersImage/free.jpg",
   },
   {
     alias: "sculpture",
-    link: "public/headersImage/sculpture.jpg",
+    link: "headersImage/sculpture.jpg",
   },
   {
     alias: "scenery",
-    link: "public/headersImage/scenery.jpg",
+    link: "headersImage/scenery.jpg",
   },
   {
     alias: "life",
-    link: "public/headersImage/life.jpg",
+    link: "headersImage/life.jpg",
   },
   {
     alias: "experiments",
-    link: "public/headersImage/experiments.jpg",
+    link: "headersImage/experiments.jpg",
   },
 ];
